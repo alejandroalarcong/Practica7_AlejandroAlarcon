f7678132b8812ea60539601afc9f33249635d2e3
alejandroalarcong <71942446+alejandroalarcong@users.noreply.github.com>
2020-11-23 22:12:27 -0600

    Practica7_AlejandroAlarcon

---
+++ Practica7_AlejandroAlarcon.c
@@ -0,0 +1,19 @@
+//Practica7                           Alarcon Gomez Alejandro
+Fecha de entrega: 23 de noviembre de 2020
+
+/*Programa para multiplicar*/
+#include<stdio.h>
+int main()
+{
+	int cantidad1, cantidad2, total;
+	 //Introducir cantidad1 para multiplicar
+	 printf("Digite la cantidad1: ");
+	 scanf("%d", &cantidad1);
+	 //Introducir cantidad2 para multiplicar
+	 printf("Digite la cantidad2: ");
+	 scanf("%d", &cantidad2);
+	 
+	 total=cantidad1*cantidad2;
+	 printf("La multiplicacion es: %d", total);
+	 
+	 return 0;}
